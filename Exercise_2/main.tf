4fda56b3d463b1280aa3a42cd5e752478551e5d3
abdennour <mail@abdennoor.com>
2020-02-12 22:34:00 +0300

    feat: done with Execrise_2

---
+++ Exercise_2/main.tf
@@ -0,0 +1,39 @@
+data "archive_file" "lambda_zip_file_greeting" {
+  type        = "zip"
+  output_path = "/tmp/lambda_zip_file_greeting.zip"
+  source {
+    content  = file("greet_lambda.py")
+    filename = "greet_lambda.py"
+  }
+}
+
+resource "aws_lambda_function" "lambda_file_greeting" {
+  filename         = "${data.archive_file.lambda_zip_file_greeting.output_path}"
+  source_code_hash = "${data.archive_file.lambda_zip_file_greeting.output_base64sha256}"
+	function_name = "greet_lambda"
+  role = "${aws_iam_role.lambda_exec_role.arn}"
+  description = "Python Lambda Function Saying Greetings"
+  handler = "greet_lambda.lambda_handler"
+  runtime = "python3.7"
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name        = "lambda_exec"
+  path        = "/"
+  description = "Allows Lambda Function to call AWS services on your behalf."
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
--- Exercise_2/variables.tf
+++ Exercise_2/variables.tf
@@ -1,1 +1,5 @@
 # TODO: Define the variable for aws_region
+# default values of VARS are populated by .env file, forwarded by docker-compose
+variable "region" {}
+# reference to Cloud credentials - default value comes from .env file
+variable "aws_profile" {}
